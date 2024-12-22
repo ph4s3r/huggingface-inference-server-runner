7c77a9bf2ad1d33ec557b39bfd516245009fbc10
ph4s3r <peterkaracsonyi85@gmail.com>
2024-12-23 00:49:08 +0100

    add idea to cpu based runner

--- stella-cpu.ps1
+++ stella-cpu.ps1
@@ -9,6 +9,8 @@
 
 # There are two problems with running this on CPU (reasons why we created the quest)
 # 1) Could not start Candle backend: Could not start backend: Qwen2 is only supported on Cuda devices in fp16 with flash attention enabled
+# an idea to try: https://github.com/huggingface/text-embeddings-inference?tab=readme-ov-file#docker-images
+[Environment]::SetEnvironmentVariable("USE_FLASH_ATTENTION", "False", "Machine")
 # 2) the onnx download also had some issues - so it would need to be downloaded with the transformers downloader and exported
 $image = "ghcr.io/huggingface/text-embeddings-inference:cpu-1.6-grpc"
 
